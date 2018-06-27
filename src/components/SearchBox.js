2bdd501fff31ae7d73c7d103a463cae1dd1132d1
llinares <lean8086@gmail.com>
2018-06-26 21:21:31 -0300

    Change selectcontainer name

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import SelectContainer from '../containers/SelectContainer';
+import Select from '../containers/SelectContainer';
 
 const SearchBox = ({ day, section, radius, price, openNow }) => (
   <section>
     <p>
-      <SelectContainer type="day" value={day} />
+      <Select type="day" value={day} />
       <span>, Id like to get some </span>
-      <SelectContainer type="section" value={section} />
+      <Select type="section" value={section} />
       <span>, ideally without walking more than </span>
-      <SelectContainer type="radius" value={radius} />
+      <Select type="radius" value={radius} />
       <span> from here.</span>
     </p>
     <p>
       <span>Show me </span>
-      <SelectContainer type="price" value={price} />
+      <Select type="price" value={price} />
       <span> venues that </span>
-      <SelectContainer type="openNow" value={openNow} />
+      <Select type="openNow" value={openNow} />
       <span>.</span>
     </p>
     <p>params: {JSON.stringify({ day, section, radius, price, openNow })}</p>
