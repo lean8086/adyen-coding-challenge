f044f56cacef700230e38fc4ddb5cb3e6f1490c2
Leandro Linares <leandro.linares@mercadolibre.com>
2018-06-27 16:49:44 -0300

    Add venue data

--- src/components/Venue.js
+++ src/components/Venue.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Venue = ({ location, name }) => (
+const Venue = ({ location, name, categories, distance }) => (
   <article>
     <p>name: {name}</p>
-    <p>loc: {location.address} - {location.lat}, {location.lng}</p>
+    <p>loc: {location.address}</p>
+    <p>ll: {location.lat}, {location.lng}</p>
+    <p>categories: {categories.map(cat => cat.name)}</p>
+    <p>distance: {distance}m</p>
   </article>
 );
 
