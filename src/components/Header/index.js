096d1692505de5bc299d86d8fda1eb9ec700f3fc
llinares <lean8086@gmail.com>
2018-09-25 16:03:31 -0300

    Add github ribbon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,9 @@ const Header = () => (
   <header className="banner" role="banner">
     <div className="innerContainer">
       <h1 className="banner__text">Human-readable Foursquare filters</h1>
+      <a href="https://github.com/lean8086/human-readable-foursquare-filters">
+        <img className="banner__img" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"/>
+      </a>
     </div>
   </header>
 );
