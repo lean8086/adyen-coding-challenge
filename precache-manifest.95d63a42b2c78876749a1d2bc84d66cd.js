9ddb4de27864a49b82ef68163aa0f70ded4372b1
lean8086 <leandro.linares@mercadolibre.com>
2019-02-02 11:39:41 -0300

    Updates

---
+++ precache-manifest.95d63a42b2c78876749a1d2bc84d66cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18882bce0d5801c8da13",
+    "url": "/human-readable-foursquare-filters/static/js/runtime~main.18882bce.js"
+  },
+  {
+    "revision": "da0cc368f1755185b95f",
+    "url": "/human-readable-foursquare-filters/static/js/main.da0cc368.chunk.js"
+  },
+  {
+    "revision": "7eb3baffa18925af0c6e",
+    "url": "/human-readable-foursquare-filters/static/js/1.7eb3baff.chunk.js"
+  },
+  {
+    "revision": "da0cc368f1755185b95f",
+    "url": "/human-readable-foursquare-filters/static/css/main.ef030abe.chunk.css"
+  },
+  {
+    "revision": "7c4bb76da7d0d1c67ed41f67dce33d9c",
+    "url": "/human-readable-foursquare-filters/index.html"
+  }
+];
